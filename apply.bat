8e633659505420fa2c30a3caf963041a90284d2c
Kaede <mutm0911@outlook.com>
2025-07-02 08:33:07 +0800

    Bug fix

---
+++ apply.bat
@@ -0,0 +1,6 @@
+xcopy ".\assets\" ".\docs\assets\" /E /Y
+xcopy ".\js\" ".\docs\js\" /E /Y
+xcopy ".\css\" ".\docs\css\" /E /Y
+xcopy ".\pages\" ".\docs\pages\" /E /Y
+xcopy ".\index.html" ".\docs\index.html" /Y
+xcopy ".\favicon.png" ".\docs\favicon.png" /Y
