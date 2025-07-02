d2bef5038006a8fc57315d15f5e77f2e0013a03c
Kaede <mutm0911@outlook.com>
2025-07-03 03:04:32 +0800

    Update: Add 2 buttons & English Page

--- js/index.js
+++ js/index.js
@@ -1,59 +1,21 @@
-fetch('../pages/header.html')
-    .then(response => response.text())
-    .then(html => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-        const headerBar = doc.querySelector('#MCG-header').content.cloneNode(true);
-        document.getElementById('header').appendChild(headerBar);
+import { init } from "./init.js";
 
-        init();
-    })
-    .catch(error => console.error('Header loading failed') );
+init();
 
-function init()
-{
-    initDropdownMenus();
-    initThemeSwitcher();
-}
-
-function initDropdownMenus()
-{
-    // Processing logic for dropdown menus
-    document.querySelectorAll('.dropdown-button').forEach(button => {
-        button.addEventListener('click', function(event)
-        {
-            event.stopPropagation();
-            const menu = this.nextElementSibling;
-            const isOpen = !menu.classList.contains('hide');
-            document.querySelectorAll('.dropdown-menu').forEach(menu => menu.classList.add('hide'));
-
-            if (!isOpen) menu.classList.remove('hide');
-        });
+document.addEventListener('DOMContentLoaded', () => {
+  // Listener for give button
+  const btnGive = document.getElementById('btn-give');
+  if (btnGive) {
+    btnGive.addEventListener('click', () => {
+      window.location.href = '../pages/give.html'; // 跳转到give页面
     });
-    document.addEventListener('click', function()
-    {
-        document.querySelectorAll('.dropdown-menu').forEach(menu => menu.classList.add('hide'));
-    });
-}
+  }
 
-function initThemeSwitcher()
-{
-    // Processing logic for theme switching button
-    document.querySelector(".display-mode-button").addEventListener('click', function()
-    {
-        document.body.classList.toggle('dark-mode');
-        const btn = document.querySelector('.display-mode-button');
-        const sun = btn.querySelector('.light-mode');
-        const moon = btn.querySelector('.dark-mode');
-        if (document.body.classList.contains('dark-mode'))
-        {
-            sun.classList.add('hide');
-            moon.classList.remove('hide');
-        }
-        else
-        {
-            sun.classList.remove('hide');
-            moon.classList.add('hide');
-        }
+  // Listener for locate button
+  const btnLocate = document.getElementById('btn-locate');
+  if (btnLocate) {
+    btnLocate.addEventListener('click', () => {
+      window.location.href = '../pages/locate.html'; // 跳转到locate页面
     });
-}
+  }
+});
